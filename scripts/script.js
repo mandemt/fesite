3fecffea82136d49b5e6ddc226cdd8209433fb88
mandemt <70643363+mandemt@users.noreply.github.com>
2020-10-22 17:27:52 +0200

    alles

--- scripts/script.js
+++ scripts/script.js
@@ -22,23 +22,70 @@ sluitKnop.addEventListener('click', sluiten);
 
 
 
-var terugKnop = document.querySelector('main section button:first-of-type');
-var heenKnop = document.querySelector('main section button:nth-of-type(2)');
-var afbeelding1 = document.querySelector('main div');
-var afbeelding2 = document.querySelector('main div:nth-of-type(2)');
-
-terugKnop.addEventListener('click',vorige);
-heenKnop.addEventListener('click',volgende);
 
 
-function volgende() {
-	console.log('heen');
-	afbeelding1.classList.add('verschijn');
-	// afbeelding2.clas sList.add('verdwijn');
+var favorietKnop = document.querySelector('.product button:first-of-type');
+var favorietPlaatje = document.querySelector('.product img:nth-of-type(2)');
+
+
+function favoriet(){
+	console.log('favoriet')
+	favorietPlaatje.classList.toggle('favoriet');
+
 }
 
-function vorige(){
-	console.log('terug');
-	afbeelding2.classList.add('verschijn');
-	afbeelding1.classList.add('verdwijn');
+favorietKnop.addEventListener('click',favoriet);
+
+
+var wegisweg1 = document.querySelector('.product>img');
+var vergrootKnop = document.querySelector('.product button:nth-of-type(2)');
+function vergroten(){
+	console.log('vergroot')
+	wegisweg1.classList.toggle('vergroten');
 }
+vergrootKnop.addEventListener('click', vergroten);
+
+
+var klein1 = document.querySelector('.product ol li:first-of-type');
+var klein2 = document.querySelector('.product ol li:nth-of-type(2)');
+var klein3 = document.querySelector('.product ol li:nth-of-type(3)');
+var klein4 = document.querySelector('.product ol li:nth-of-type(4)');
+var klein5 = document.querySelector('.product ol li:nth-of-type(5)');
+
+function andereFoto(){
+	wegisweg1.src= "images/wegisweg1.png"
+}
+
+klein1.addEventListener('click',andereFoto);
+
+
+function andereFoto2(){
+	wegisweg1.src= "images/wegisweg2.png"
+}
+
+klein2.addEventListener('click',andereFoto2);
+
+function andereFoto3(){
+	wegisweg1.src= "images/wegisweg3.png"
+}
+
+klein3.addEventListener('click',andereFoto3);
+
+function andereFoto4(){
+	wegisweg1.src= "images/wegisweg4.png"
+}
+
+klein4.addEventListener('click',andereFoto4);
+
+function andereFoto5(){
+	wegisweg1.src= "images/wegisweg5.png"
+}
+
+klein5.addEventListener('click',andereFoto5);
+
+
+
+
+
+
+
